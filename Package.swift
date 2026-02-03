fa71abd58029efcfc3d97e5fd9987644cc5dfd8d
Gijs Meuldijk <gijs@gijsbertus.com>
2026-02-03 14:10:00 +0100

    Update Swift tools version and iOS platform version
    Updated the Swift tools version and minimum iOS platform version.

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Ometria",
-    platforms: [.iOS(.v11)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "Ometria",
